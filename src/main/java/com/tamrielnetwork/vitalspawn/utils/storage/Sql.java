932f60fcd6e00a90eb7c2c698de1c20bca8a9489
TamrielNetwork <contact@tamriel.me>
2022-03-07 05:58:01 +0100

    Update Copyright to feature contributors

--- src/main/java/com/tamrielnetwork/vitalspawn/utils/storage/Sql.java
+++ src/main/java/com/tamrielnetwork/vitalspawn/utils/storage/Sql.java
@@ -1,6 +1,6 @@
 /*
  * VitalSpawn is a Spigot Plugin that lets you set a spawn point.
- * Copyright © 2022 Leopold Meinel
+ * Copyright © 2022 Leopold Meinel & contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
