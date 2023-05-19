72b6113784595fe8c1dc629d0140c8843971c9ad
Leopold Johannes Meinel <leo@meinel.dev>
2023-05-19 22:38:20 +0200

    Use non-deprecated getPluginMeta()

--- src/main/java/dev/meinel/leo/vitalspawn/VitalSpawn.java
+++ src/main/java/dev/meinel/leo/vitalspawn/VitalSpawn.java
@@ -2,7 +2,7 @@
  * File: VitalSpawn.java
  * Author: Leopold Meinel (leo@meinel.dev)
  * -----
- * Copyright (c) 2022 Leopold Meinel & contributors
+ * Copyright (c) 2023 Leopold Meinel & contributors
  * SPDX ID: GPL-3.0-or-later
  * URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
  * -----
@@ -36,7 +36,7 @@ public final class VitalSpawn extends JavaPlugin {
         messages = new Messages();
         Bukkit
                 .getLogger()
-                .info("VitalSpawn v" + this.getDescription().getVersion() + " enabled");
+                .info("VitalSpawn v" + this.getPluginMeta().getVersion() + " enabled");
         Bukkit.getLogger().info("Copyright (C) 2022 Leopold Meinel");
         Bukkit.getLogger().info("This program comes with ABSOLUTELY NO WARRANTY!");
         Bukkit
@@ -53,7 +53,7 @@ public final class VitalSpawn extends JavaPlugin {
     public void onDisable() {
         Bukkit
                 .getLogger()
-                .info("VitalSpawn v" + this.getDescription().getVersion() + " disabled");
+                .info("VitalSpawn v" + this.getPluginMeta().getVersion() + " disabled");
     }
 
     private void setupStorage() {
